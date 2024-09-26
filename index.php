e5af2078e95dd585adedc4939fed0117127c9682
jjybaliguat <justinejeraldbaliguat@gmail.com>
2024-09-26 14:31:24 +0800

    some changes

--- index.php
+++ index.php
@@ -1,26 +1,64 @@
 <?php
 require('utils/ovrf-generator.php');
 
+function getEnrolledCourses(){
+    return [
+        [
+            'code'=>'OJT 240',
+            'subjectDescription'=>'On the Job Training (240 Hours)',
+            'yearSec'=>'',
+            'units'=>'3',
+            'days'=>'',
+            'time'=>'',
+            'room'=>'',
+        ],
+        [
+            'code'=>'OJT 240',
+            'subjectDescription'=>'On the Job Training (240 Hours)',
+            'yearSec'=>'',
+            'units'=>'3',
+            'days'=>'',
+            'time'=>'',
+            'room'=>'',
+        ],
+        [
+            'code'=>'OJT 240',
+            'subjectDescription'=>'On the Job Training (240 Hours)',
+            'yearSec'=>'',
+            'units'=>'3',
+            'days'=>'',
+            'time'=>'',
+            'room'=>'',
+        ],
+        [
+            'code'=>'OJT 240',
+            'subjectDescription'=>'On the Job Training (240 Hours)',
+            'yearSec'=>'',
+            'units'=>'3',
+            'days'=>'',
+            'time'=>'',
+            'room'=>'',
+        ],
+    ];
+}
+
+$enrolledCourses = getEnrolledCourses();
+$isNewStudent = false;
+
 $ovrf = new Student_OVRF();
-$ovrf->setName('Compra', 'Iza', 'M');
-$ovrf->setStudentNumber('22-00760');
-$ovrf->setCourse('BSBA');
+$ovrf->setName('Baliguat', 'Justine Jerald', 'Y');
+$ovrf->setStudentNumber('21-01530');
+$ovrf->setCourse('BSCPE');
 $ovrf->setAcademicYear('2023-2024');
 $ovrf->setSemester('summer');
-$ovrf->setYearLevel('1');
+$ovrf->setYearLevel('4');
 $ovrf->setComputerFee('0.00');
 $ovrf->setLaboratoryFee('0.00');
-$ovrf->setSubjects(Array(
-    array(
-        'code'=>'OJT 240',
-        'subjectDescription'=>'On the Job Training (240 Hours)',
-        'yearSec'=>'',
-        'units'=>'3',
-        'days'=>'',
-        'time'=>'',
-        'room'=>'',
-    ),
-));
+if($isNewStudent){
+    $ovrf->setSchoolIdFee('0.00');
+    $ovrf->setAdmissionFee('0.00');
+}
+$ovrf->setSubjects($enrolledCourses);
 
 $ovrf->generateOvrf();
 
